b9d3935cc325510c73ff484f749e1f3cfbf1167c
Yuto Tamura <cayt1224@gmail.com>
2023-10-10 21:41:11 +0900

    課題作成

---
+++ main.js
@@ -0,0 +1,33 @@
+//AND(&&)
+var numA = 10;
+var numB = 20;
+if (numA === 10 && numB === 20) {
+  console.log("true処理");
+  console.log(numA === 10 && numB === 20);
+} else {
+  console.log("false処理");
+  console.log(numA === 10 && numB === 20);
+}
+
+
+//OR(||)
+var numA = 10;
+var numB = 20;
+if (numA === 10 || numB === 20) {
+  console.log("true処理");
+  console.log(numA === 10 || numB === 20);
+} else {
+  console.log("false処理");
+  console.log(numA === 10 || numB === 20);
+}
+
+
+//NOT(!)
+var num = 123;
+if (num != 100) {
+  console.log("true処理");
+  console.log(num);
+} else {
+  console.log("false処理");
+  console.log(num);
+}
